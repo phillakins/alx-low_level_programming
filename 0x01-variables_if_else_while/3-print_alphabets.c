1fc7df483591598f617a3ebc2af8d32f730093dc
Philip Akinshipe <pakinshipe@gmail.com>
2022-10-14 22:15:08 +0000

    Adding 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char alp[54] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 54; i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
