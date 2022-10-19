2e7817b34b8764219ea70560810712dc4130484a
Philip Akinshipe <pakinshipe@gmail.com>
2022-10-19 00:43:15 +0000

    adding task 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ *
+ * @r: char type integer.
+ *
+ * Return: absolute value of an integer
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+	_putchar('\n')
+}
