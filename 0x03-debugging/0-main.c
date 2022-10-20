08465dd0ec137a2fc11658224e76d376e519c839
Philip Akinshipe <pakinshipe@gmail.com>
2022-10-20 20:08:57 +0000

    adding task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -8,8 +9,8 @@ int main(void)
 {
 	int i;
 
-	i = 98;
-	putchar(i);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
