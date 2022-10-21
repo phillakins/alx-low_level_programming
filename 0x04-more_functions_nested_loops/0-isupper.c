ebe0799b32a687d2d0f3f174ab7109a94b937c99
Philip Akinshipe <pakinshipe@gmail.com>
2022-10-21 20:22:32 +0000

    Addig Task 0. All by myself

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character.
+ *
+ *@c: char type alphabet
+ *
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
