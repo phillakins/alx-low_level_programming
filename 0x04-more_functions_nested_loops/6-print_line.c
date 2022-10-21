ed855ffef75b60488395c052b84c7c6f8d998691
Philip Akinshipe <pakinshipe@gmail.com>
2022-10-21 23:06:58 +0000

    Addig task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  print_line -  function that draws a straight line in the terminal.
+ *
+ *  Return: always 0
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
