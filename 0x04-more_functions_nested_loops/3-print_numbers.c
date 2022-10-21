c0acf75d4fa30f9bee747789805ea3f95ad74eaf
Philip Akinshipe <pakinshipe@gmail.com>
2022-10-21 21:06:50 +0000

    adding task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - a function that prints the numbers, from 0 to 9.
  *
- * Return: Always 0
+ * Return: no return
  */
 void print_numbers(void)
 {
@@ -12,7 +12,6 @@ void print_numbers(void)
 	for (a = 0; a < 10; a++)
 	{
 		_putchar(a);
-		return (0);
 	}
 	_putchar('\n');
 }
