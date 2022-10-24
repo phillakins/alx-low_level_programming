d1b83fe098caec075e2b03eb8e5c216581a40b54
Philip Akinshipe <pakinshipe@gmail.com>
2022-10-24 21:17:32 +0000

    Adding task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  function that takes a pointer and updates the value
+ *@n: char type integer
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning Strings, Arrays and Pointers
